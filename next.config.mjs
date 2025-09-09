010da8bf76462d5ffa905048f9d4392d0fd0bfd9
Sdg877 <sdrakegill@gmail.com>
2025-09-09 09:55:24 +0100

    Update to include volunteer url.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/do-it/:path*',
+        destination: 'https://api.do-it.org/:path*',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
