cd7491c3ebef990543a0edf9dbbe6042329b4f73
Sdg877 <sdrakegill@gmail.com>
2025-09-18 08:11:57 +0100

    Old code removed.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,32 +1,3 @@
-// import { DM_Sans } from 'next/font/google'
-// import Navbar from '../components/Navbar'; // 👈 Import the Navbar component
-
-// const dmSans = DM_Sans({
-//   subsets: ['latin'],
-//   weight: ['400', '500', '700'],
-//   display: 'swap',
-// })
-
-// export const metadata = {
-//   title: 'Carbon Calculator',
-//   description: '...',
-// }
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <body className={dmSans.className}>
-//         <header>
-//           <Navbar /> 
-//         </header>
-//         <main>
-//           {children}
-//         </main>
-//       </body>
-//     </html>
-//   )
-// }
-
 import { DM_Sans } from 'next/font/google'
 import Navbar from '../components/Navbar'
 import AuthWrapper from '../components/AuthWrapper'
