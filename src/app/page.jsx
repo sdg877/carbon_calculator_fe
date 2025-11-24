9e03fc3ca5eea5b2d6ebd337f52b912305601f72
Sdg877 <sdrakegill@gmail.com>
2025-11-24 07:07:19 +0000

    Update to dashboard wording for clarity.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,17 +9,17 @@ export default function HomePage() {
       <section className={styles.introductionSection}>
         <p className={styles.paragraph}>
           Welcome to the Carbon Calculator. This app helps you track your
-          personal carbon footprint based on your daily activities—like
+          personal carbon footprint based on your daily activities, like
           transport, energy use, food, shopping, and lifestyle choices.
         </p>
         <p className={styles.paragraph}>
-          By logging your activities, you can see how much **CO**<sub>2</sub>{" "}
-          you produce and get **personalised suggestions** for reducing your
+          By logging your activities, you can see how much CO<sub>2</sub>{" "}
+          you produce and get suggestions for reducing your
           impact on the environment. Earn points for completing eco-friendly
           actions and track your monthly progress.
         </p>
         <p className={styles.paragraph}>
-          Start by **logging in or registering** to create your account, and
+          Start by logging in or registering to create your account, and
           begin your journey toward a more sustainable lifestyle.
         </p>
       </section>
