0690fa38ac00e04ebb93bd9556768d6c6e7e6ae4
Sdg877 <sdrakegill@gmail.com>
2025-11-24 07:33:35 +0000

    Update to font family.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,10 @@
-import { Roboto_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "../styles/forms.css";
 import AuthWrapper from "../components/AuthWrapper.js";
 import Navbar from "../components/Navbar.jsx";
 import ModalRoot from "../components/ModalRoot.jsx";
 
-const roboto = Roboto_Mono({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "CarbonCalc - Track Your Footprint",
@@ -15,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={roboto.className} suppressHydrationWarning={true}>
+      <body className={inter.className} suppressHydrationWarning={true}>
         <AuthWrapper>
           <Navbar />
           <main className="min-h-screen">{children}</main>
