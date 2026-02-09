33fc3a4cdd185c5fd0e9e903230bb319cb448f83
Sdg877 <sdrakegill@gmail.com>
2026-02-09 12:42:33 +0000

    Implement barrel exports for components and helpers

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Navbar } from './Navbar';
+export { default as Footer } from './Footer';
+export { default as LogoutButton } from './LogoutButton';
+export { default as Modal } from './Modal';
+export { default as AuthWrapper } from './AuthWrapper';
