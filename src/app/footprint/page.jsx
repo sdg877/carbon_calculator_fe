7107500eff86fd81e5b1ba497852c3bec6369318
Sdg877 <sdrakegill@gmail.com>
2026-02-09 12:43:05 +0000

    Implement barrel exports for components and helpers

--- src/app/footprint/page.jsx
+++ src/app/footprint/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import Modal from "../../components/Modal.jsx";
+import { Modal } from '@/components';
 import "../../styles/forms.css";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
